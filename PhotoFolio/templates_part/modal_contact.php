380b332bec05edeae336f00d211c2a86ef92432c
bendito74 <154443655+bendito74@users.noreply.github.com>
2024-04-11 14:54:35 +0200

    mise en forme de la modal avec le css php et js

--- PhotoFolio/templates_part/modal_contact.php
+++ PhotoFolio/templates_part/modal_contact.php
@@ -1,3 +1,4 @@
 <div id="modal-contact" class="modal hidden">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/contact.png" alt="le mot contact en répétition pour un effet de style">
     <?php echo do_shortcode('[contact-form-7 id="c242556" title="Formulaire de contact 1"]'); ?>
 </div>
