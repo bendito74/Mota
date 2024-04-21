e1a2dbb2b2b2283194b7fc565a87dd09ef47365b
bendito74 <154443655+bendito74@users.noreply.github.com>
2024-04-21 14:27:54 +0200

    Changement sur la modal, ajout d'un overlay et stylysation + responsive

--- PhotoFolio/assets/js/script.js
+++ PhotoFolio/assets/js/script.js
@@ -3,68 +3,86 @@ document.addEventListener('DOMContentLoaded', function() {
     //MODALE DE CONTACT POPUP
 var contactButton = document.querySelector('.menu-item.contact-button a');
 var singleContactButton = document.getElementById("single-contact_left-button");
-var modal = document.getElementById('modal-contact');
-var inputField = modal.querySelector('#wp7-reference'); // Remplacez #input-field par le sélecteur de votre champ de formulaire
+var modal = document.querySelector('.modal-container'); // Correction ici
+var modalOverlay = document.querySelector('.modal-overlay');
+var modalContent = document.querySelector('.modal');
+var inputField = modalContent.querySelector('#wp7-reference'); // Correction ici
 var referenceSpan = document.getElementById('reference');
-var referenceValue = referenceSpan.textContent;
+var referenceValue = referenceSpan ? referenceSpan.textContent : '';
 
-    // Ouvre la modal quand le bouton "Contact" est cliqué
-    contactButton.addEventListener('click', function(event) {
-        event.preventDefault(); // Empêche le comportement par défaut du lien
-    
-    // Réinitialiser la valeur du champ de formulaire
-        inputField.value = ""; // Effacer la valeur préremplie
-    
-        modal.classList.remove('hidden');
-    });
-
-    // Ouvre la modal quand le bouton "Contact" dans single.php est cliqué
-    singleContactButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Empêche le comportement par défaut du lien
-    
-    // Préremplir le champ de formulaire
-        inputField.value = referenceValue; // Remplacez "Votre valeur préremplie" par la valeur souhaitée
-    
-        modal.classList.remove('hidden');
-    });
-
-    // Ferme la modal quand on clique en dehors d'elle
-    document.addEventListener('click', function(event) {
-        if (!modal.contains(event.target) && event.target !== contactButton && event.target !== singleContactButton) {
-            modal.classList.add('hidden');
-        }
-    });
-
-
-
-        //COMPORTEMENT DES PHOTOS ET FLECHES DANS SINGLE.PHP
-    let imgPrecedent = document.querySelector('.post-image.precedent');
-    let imgSuivant = document.querySelector('.post-image.suivant');
-    let arrowPrevious = document.querySelector('.arrow.previous');
-    let arrowNext = document.querySelector('.arrow.next');
-
-    // Fonction pour changer l'opacité d'une image
-    function changeOpacity(element, opacityValue) {
-        element.style.opacity = opacityValue;
+// Fonction pour fermer la modal quand on clique en dehors d'elle
+function closeOnClickOutside(event) {
+    if (!modal.contains(event.target) && event.target !== contactButton && event.target !== singleContactButton) {
+        modal.classList.add('hidden');
+        document.removeEventListener('click', closeOnClickOutside); // Désactive l'écouteur d'événement après la fermeture
     }
+}
 
-    // Gestionnaire d'événement pour arrowPrevious
-    arrowPrevious.addEventListener('mouseenter', function() {
-        changeOpacity(imgPrecedent, '1');
-    });
+// Ouvre la modal quand le bouton "Contact" dans le menu_nav est cliqué
+contactButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
 
-    arrowPrevious.addEventListener('mouseleave', function() {
-        changeOpacity(imgPrecedent, '0');
-    });
+    // Réinitialiser la valeur du champ de formulaire
+    inputField.value = ""; // Effacer la valeur préremplie
 
-    // Gestionnaire d'événement pour arrowNext
-    arrowNext.addEventListener('mouseenter', function() {
-        changeOpacity(imgSuivant, '1');
-    });
+    modal.classList.remove('hidden');
 
-    arrowNext.addEventListener('mouseleave', function() {
-        changeOpacity(imgSuivant, '0');
+    // Ferme la modal quand on clique sur l'overlay
+    modalOverlay.addEventListener('click', function(event) {
+        modal.classList.add('hidden');
+        document.removeEventListener('click', closeOnClickOutside); // Désactive l'écouteur d'événement après la fermeture
     });
+});
+
+// Ouvre la modal quand le bouton "Contact" dans single.php est cliqué
+singleContactButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+
+    // Préremplir le champ de formulaire
+    inputField.value = referenceValue; // Remplacez "Votre valeur préremplie" par la valeur souhaitée
+
+    modal.classList.remove('hidden');
+
+    // Ferme la modal quand on clique sur l'overlay
+    modalOverlay.addEventListener('click', function(event) {
+        modal.classList.add('hidden');
+        document.removeEventListener('click', closeOnClickOutside); // Désactive l'écouteur d'événement après la fermeture
+    });
+});
+
+
+
+
+//COMPORTEMENT DES PHOTOS ET FLECHES DANS SINGLE.PHP
+let imgPrecedent = document.querySelector('.post-image.precedent');
+let imgSuivant = document.querySelector('.post-image.suivant');
+let arrowPrevious = document.querySelector('.arrow.previous');
+let arrowNext = document.querySelector('.arrow.next');
+
+// Fonction pour changer l'opacité d'une image
+function changeOpacity(element, opacityValue) {
+    element.style.opacity = opacityValue;
+}
+
+// Gestionnaire d'événement pour arrowPrevious
+arrowPrevious.addEventListener('mouseenter', function() {
+    changeOpacity(imgPrecedent, '1');
+});
+
+arrowPrevious.addEventListener('mouseleave', function() {
+    changeOpacity(imgPrecedent, '0');
+});
+
+// Gestionnaire d'événement pour arrowNext
+arrowNext.addEventListener('mouseenter', function() {
+    changeOpacity(imgSuivant, '1');
+});
+
+arrowNext.addEventListener('mouseleave', function() {
+    changeOpacity(imgSuivant, '0');
+});    
+
+        
 
 
 });
--- PhotoFolio/templates_part/modal_contact.php
+++ PhotoFolio/templates_part/modal_contact.php
@@ -1,4 +1,7 @@
-<div id="modal-contact" class="modal hidden">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/contact.png" alt="le mot contact en répétition pour un effet de style">
-    <?php echo do_shortcode('[contact-form-7 id="c242556" title="Formulaire de contact 1"]'); ?>
+<div id="modal-contact" class="modal-container hidden">
+    <div class="modal-overlay"></div>
+    <div class="modal">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/contact.png" alt="le mot contact en répétition pour un effet de style">
+        <?php echo do_shortcode('[contact-form-7 id="c242556" title="Formulaire de contact 1"]'); ?>
+    </div>
 </div>
