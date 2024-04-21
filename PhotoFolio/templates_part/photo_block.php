6a0c2e9911ab1e0ecf64a2bb22d9095aecd21583
bendito74 <154443655+bendito74@users.noreply.github.com>
2024-04-21 17:53:46 +0200

    style et code pour l'overlay du photoblock

--- PhotoFolio/templates_part/photo_block.php
+++ PhotoFolio/templates_part/photo_block.php
@@ -14,6 +14,24 @@ function display_two_photos($args_photos) {
                 $div_class = ($counter == 0) ? 'photo-block_left' : 'photo-block_right';
                 echo '<div class="' . $div_class . '">';
                 echo wp_get_attachment_image($image_id, 'full');
+                // Ajout de l'overlay
+                echo '<div class="overlay">';
+                //afficher le logo plein écran
+                echo '<img src="' . get_template_directory_uri() . '/assets/img/pleinEcran.png" alt="logo représentant un carré dans un rond et servant à agrandir l\'image" class="pleinEcran-photo">';
+                //afficher le logo oeil
+                echo '<img src="' . get_template_directory_uri() . '/assets/img/oeil.png" alt="logo en forme d\'oeil permettant d\'afficher le descriptif de la photo" class="oeil-photo">';
+                //afficher le titre
+                echo '<span class="title-photo">';
+                the_title(); 
+                echo '</span>';
+                // afficher la catégorie
+                $format_names = wp_list_pluck(get_the_terms(get_the_ID(), 'categorie'), 'name');
+                if (!empty($format_names)) {
+                echo '<span class="category-photo">';
+                echo implode(', ', $format_names);
+                echo '<span>';
+                }
+                echo '</div>';
                 echo '</div>';
                 $counter++;
             }
